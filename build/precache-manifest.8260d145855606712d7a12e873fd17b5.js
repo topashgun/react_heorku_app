340e3107999c22e917b04eba237013054645e036
topashgun <topashgun@gmail.com>
2020-06-23 22:39:20 +0530

    initial commit

---
+++ build/precache-manifest.8260d145855606712d7a12e873fd17b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f76c8547c1976b3e86a144bf971ea19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e561947370632e60a6c1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f528340de77abaf92b18",
+    "url": "/static/js/2.98914163.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.98914163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e561947370632e60a6c1",
+    "url": "/static/js/main.385250bb.chunk.js"
+  },
+  {
+    "revision": "a3d2bf21716328fc74a3",
+    "url": "/static/js/runtime-main.d8499093.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
